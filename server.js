930bb0a87c1248b3d172700b900ae6f7bdce89a2
Ailiniyazi <a.maimaiti@fkc-online.com>
2020-05-19 15:09:18 +0200

    changes with greenlock

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const app = require('./lib/application')();
 const argv = require('optimist').argv;
 
+require("greenlock-express")
+    .init({
+        packageRoot: __dirname,
+
+        // contact for security and critical bug notices
+        configDir: "./greenlock.d",
+        maintainerEmail: 'a.maimaiti@fkc-online.com',
+        // whether or not to run at cloudscale
+        cluster: false
+    })
+
 app.run(argv);
